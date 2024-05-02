cd30b4a6d2b515ccf138fd2dac4757512da3aae6
luccaneves <61149862+luccaneves@users.noreply.github.com>
2024-05-02 10:07:50 -0300

    Update escon_motor_env.h
    Passadas constantes para env motor

--- lib/forecastnucleoframework/include/forecast/platforms/workbench/config/escon_motor_env.h
+++ lib/forecastnucleoframework/include/forecast/platforms/workbench/config/escon_motor_env.h
@@ -6,14 +6,21 @@ namespace envMotor{
     constexpr PinName MOTOR_ENABLE_PIN = PC_11;
     constexpr PinName MOTOR_CURRENT_FEEDBACK_PIN = PA_1;
     constexpr PinName MOTOR_ANALOG_PIN = PA_5;
-    constexpr float offset_bias = 0.00;
-    constexpr float amp_scale = 1.00;
 
-    // ------- EXO MOTOR ------------------------
-    constexpr float MAX_CURR = 2.26f;
-    constexpr float KT = 1.5038f;   
-    constexpr float JM = 0.0104f;
-    constexpr float DM = 0.4f;
+
+    constexpr float MAX_CURR = 3.30f; // is it actually the max analog voltage level?
+    
+    //constexpr float LINMOT_FORCE_MAX = 255.00;
+    //constexpr float LINMOT_FORCE_MIN = -255.00;
+    //constexpr float KT = (LINMOT_FORCE_MAX - LINMOT_FORCE_MIN);
+    
+    constexpr float HYD_CURRENT_MAX = 10.00;
+    constexpr float HYD_CURRENT_MIN = -10.00;
+    constexpr float KT = (HYD_CURRENT_MAX - HYD_CURRENT_MIN);
+    
+    constexpr float JM = 1.0000f;
+    constexpr float offset_bias = -0.02; // LinMot: 0.000 | Moog E024: -0.0195
+    constexpr float amp_scale = 1.00;
 }
 
 #endif // CONFIG_CONTROL_ESCON_MOTOR_H
