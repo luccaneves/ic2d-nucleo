b26a8055d111c2fdc6b97428bd59e92113877016
luccaneves <61149862+luccaneves@users.noreply.github.com>
2024-10-03 16:54:32 -0300

    Criado controlador compliant do Helio

---
+++ lib/forecastnucleoframework/include/forecast/controllers/CompliantHelio.hpp
@@ -0,0 +1,270 @@
+#ifndef COMP_HELIO_H
+#define COMP_HELIO_H
+
+#include <utility/filters/AnalogFilter.hpp>
+
+#include "../Controller.hpp"
+
+namespace forecast {
+
+/**
+ * @brief Feedback Linearization Control class
+ **/
+class CompliantHelio : public Controller {
+public:
+  /**
+   * @brief Construct a new Feedback Linearization object. This constructor
+   initialize,
+  * the controller.
+  * @param kp
+  * @param kd
+  * @param ki
+  * @param Kvc
+  * @param Kpc
+  * @param B_int
+  * @param leak_fix
+  * @param limit
+
+  **/
+  CompliantHelio(float max_f = 0, float min_f = 0, float max_g = 0, float min_g = 0, float etta = 0
+        , float psi = 0, float limit = 0, float gain_out = 0, float gain_dob = 0, float limit_dob = 0, 
+        float lambda = 0, float max_disturb_current = 0,float min_disturb_current = 0, float disturb_model_gain = 0, 
+        float kp = 0, float ki = 0, float kd = 0,
+                   float Kdes = 0, float Bdes = 0, float Mdes = 0,
+                   float K1 = 0, float K2 = 0, float massa_total = 0, float F_fric = 0, float psi_compliant = 0);
+
+  virtual float process(const IHardware *hw, std::vector<float> ref) override;
+
+  float ForceController(const IHardware *hw, float ref);
+
+protected:
+
+  float Kdes = 0;
+  float Bdes = 0;
+  float Mdes = 0;
+  
+  float max_f = 0;
+  float min_f = 0;
+  float max_g = 0;
+  float min_g = 0;
+  float etta = 0;
+  float psi = 0;
+  float gain_out = 0;
+  float beta = 0;
+  float gain_g_med = 0;
+  float gain_f_med = 0;
+  float max_disturb_current = 0;
+  float min_disturb_current = 0;
+  float disturb_model_gain = 0;
+
+  float kp = 0.0;
+  float kd = 0.0;
+  float ki = 0.0f;
+  float offset_x = 0;
+  float once = 1;
+  float once_force = 1;
+  float filter_out = 0;
+
+  float Kvc = 0.0f;
+  float Kpc = 0.0f;
+  float Ml = 0;
+  float Kl = 0;
+
+  float tau = 0.0f;
+  float dtau = 0.0f;
+  float x = 0.0f;
+  float dx = 0.0f;
+  float ddx = 0;
+  float z = 0;
+  float aux1 = 0;
+  float aux2 = 0;
+
+  float prev_ref_1 = 0.0;
+  float prev_ref_2 = 0.0;
+  float prev_ref_3 = 0.0;
+  float prev_ref_4 = 0.0;
+  float prev_ref_5 = 0.0;
+  float prev_ref_6 = 0.0;
+  float prev_ref_7 = 0.0;
+
+  float err = 0.0;
+  float derr = 0.0;
+  float ierr = 0.0;
+
+  float errPast = 0.0;
+
+  float out;
+  float reference = 0.0;
+
+  float Be = 0.0f;
+  float De = 0.0f;
+  float Dh = 0.0f;
+  float L_cyl = 0.0f;
+  float Aa = 0.0f;
+  float Ap = 0.0f;
+  float Ab = 0.0f;
+  float alfa = 0.0f;
+  float Va = 0.0f;
+  float Vb = 0.0f;
+  float Vpl = 0.0f;
+  float In = 0.0f;
+  float pn = 0.0f;
+  float qn = 0.0f;
+  float Fv = 0.0f;
+  float Kv = 0.0f;
+  float Ps = 0.0f;
+  float Pt = 0.0f;
+  float Pa = 0.0f;
+  float Pb = 0.0f;
+  float f = 0.0f;
+  float g = 0.0f;
+  float v = 0.0f;
+  float ixv = 0.0f;
+  float B_int = 0;
+  float leak_fix = 0;
+  float limit = 0;
+  float start_x = 0;
+  float d_force = 0;
+
+  float prev_erro_1 = 0.0;
+  float prev_erro_2 = 0.0;
+  float prev_erro_3 = 0.0;
+  float prev_erro_4 = 0.0;
+  float prev_erro_5 = 0.0;
+  float prev_erro_6 = 0.0;
+  float prev_erro_7 = 0.0;
+
+  double prev1_filter_exit_dx = 0;
+  double prev2_filter_exit_dx = 0;
+  double prev3_filter_exit_dx = 0;
+  double prev4_filter_exit_dx = 0;
+  double prev5_filter_exit_dx = 0;
+
+  double prev1_dx = 0;
+  double prev2_dx = 0;
+  double prev3_dx = 0;
+  double prev4_dx = 0;
+  double prev5_dx = 0;
+
+  double prev1_filter_exit_dF = 0;
+  double prev2_filter_exit_dF = 0;
+  double prev3_filter_exit_dF = 0;
+  double prev4_filter_exit_dF = 0;
+  double prev5_filter_exit_dF = 0;
+
+  double prev1_dF = 0;
+  double prev2_dF = 0;
+  double prev3_dF = 0;
+  double prev4_dF = 0;
+  double prev5_dF = 0;
+
+  double prev1_filter_exit_current = 0;
+  double prev2_filter_exit_current = 0;
+  double prev3_filter_exit_current = 0;
+  double prev4_filter_exit_current = 0;
+  double prev5_filter_exit_current = 0;
+
+  double prev1_current = 0;
+  double prev2_current = 0;
+  double prev3_current = 0;
+  double prev4_current = 0;
+  double prev5_current = 0;
+
+
+
+  double prev1_disturb1 = 0;
+  double prev2_disturb1 = 0;
+  double prev3_disturb1 = 0;
+  double prev4_disturb1 = 0;
+  double prev5_disturb1 = 0;
+  double prev6_disturb1 = 0;
+
+  float gain_vc = 0;
+  float vc_limit = 0;
+
+  float lambda = 0;
+  float gain_dob = 0;
+  float fl = 0;
+  float limit_dob = 0;
+  float disturb = 0.0003;
+  float disturb1 = 0.000;
+  float disturb2 = 0.000;
+  float disturb3 = 0.000;
+
+  float expected_force = 0.0;
+  float last_out = 0.0;
+  float dob_formulation = 0;
+  float pressure_predict = 0;
+
+  float erro_imp = 0;
+
+  float deriv_erro_imp = 0;
+
+  float last_erro_imp = 0;
+
+  float z = 0;
+  float last_z = 0;
+  float dz = 0;
+
+
+  float x_hat = 0;
+  float last_x_hat = 0;
+  float dx_hat = 0;
+  float last_dx_hat = 0;
+  float ddx_hat = 0;
+
+  float new_forca_desejada = 0;
+  float last_new_forca_desejada = 0;
+  float d_new_forca_desejada = 0;
+
+
+  //Novos parametros
+  float K1 = 0;
+  float K2 = 0;
+  float F_fric = 0;
+  float psi_compliant = 0;
+  float massa_total = 0;
+
+
+
+  utility::AnalogFilter* lowPass;
+  utility::AnalogFilter* lowPassD;
+  utility::AnalogFilter* lowPassx;
+  utility::AnalogFilter* lowPassDx;
+  utility::AnalogFilter* lowPassPs;
+  utility::AnalogFilter* lowPassPt;
+  utility::AnalogFilter* lowPassPa;
+  utility::AnalogFilter* lowPassPb;
+  utility::AnalogFilter* lowPassD_ErroImp;
+  
+  utility::AnalogFilter* lowPassD_z;
+  utility::AnalogFilter* lowPassD_Xhat;
+  utility::AnalogFilter* lowPassDD_Xhat;
+  utility::AnalogFilter* lowPassd_new_forca_desejada;
+  utility::AnalogFilter* transferFunction;
+};
+
+inline ControllerFactory::Builder make_CompliantHelio_builder() {
+
+  auto fn = [](std::vector<float> params) -> Controller * {
+    if (params.size() < 1)
+      return nullptr; // not enough parameters
+
+    return new CompliantHelio(params[0], params[1], params[2], params[3],
+                                params[4],params[5],params[6],params[7],params[8],params[9],params[10],
+                                params[11],params[12],params[13],params[14],params[15],params[16],params[17], params[18]
+                                , params[19]
+                                , params[20], params[21], params[22], params[23], params[24]);
+  };
+
+  return {
+      fn,
+      {"max_f", "min_f", "max_g", "min_g", "eta","psi", "limit","gain_out","gain_dob","limit_dob","lambda",
+      "max_disturb_current","min_disturb_current","disturb_model_gain","kp","ki","Kd","Kdes","Bdes","Mdes",
+      "K1","K2","M","F_FRIC","psi_comp"},
+      {"reference"}};
+}
+
+} // namespace forecast
+
+#endif // IFEEDBACK_LIN_H
---
+++ lib/forecastnucleoframework/src/controllers/CompliantHelio.cpp
@@ -0,0 +1,333 @@
+#include <forecast/controllers/CompliantHelio.hpp>
+
+using namespace forecast;
+
+CompliantHelio::CompliantHelio(float max_f, float min_f, float max_g, float min_g, float eta, float psi, float limit, float gain_out, float gain_dob, float limit_dob, float lambda
+,float max_disturb_current, float min_disturb_current, float disturb_model_gain, float kp, float ki, float kd, float Kdes, float Bdes,float Mdes
+,float K1, float K2, float massa_total, float psi_compliant, float F_fric)
+    : 
+      tau(0.0f),
+      dtau(0.0f),
+      errPast(0.f),
+      err(0.f),
+      derr(0.f),
+      ierr(0.f),
+      x(0.f),
+      dx(0.f),
+      Aa(0.0f),
+      Ap(0.0f),
+      Ab(0.0f),
+      alfa(0.0f),
+      Va(0.0f),
+      Vb(0.0f),
+      Kv(0.0f),
+      Pt(0.0f),
+      Ps(0.0f),
+      Pa(0.0f),
+      Pb(0.0f),
+      f(0.0f),
+      g(0.0f),
+      v(0.0f),
+      limit(limit),
+      gain_out(gain_out),
+      max_f(max_f),
+      max_g(max_g),
+      min_f(min_f),
+      min_g(min_g),
+      psi(psi),
+      etta(eta),
+      gain_dob(gain_dob),
+      limit_dob(limit_dob),
+      lambda(lambda),
+      max_disturb_current(max_disturb_current),
+      min_disturb_current(min_disturb_current),
+      disturb_model_gain(disturb_model_gain),
+      kp(kp),
+      ki(ki),
+      kd(kd),
+      Kdes(Kdes),
+      Bdes(Bdes),
+      Mdes(Mdes),
+      K1(K1),
+      K2(K2),
+      massa_total(massa_total),
+      psi_compliant(psi_compliant),
+      F_fric(F_fric)
+{
+    float freq = 40.0;
+    lowPass = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassD = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassx = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassDx = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassPa = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassPb = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassPs = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassPt = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassD_ErroImp = utility::AnalogFilter::getLowPassFilterHz(freq);
+
+    lowPassD_z = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassDD_Xhat = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassDD_Xhat = utility::AnalogFilter::getLowPassFilterHz(freq);
+    lowPassd_new_forca_desejada = utility::AnalogFilter::getLowPassFilterHz(freq);
+    
+    Be = 1.31E+9f; // Bulk modulus [Pa]
+    De = 0.016f;  // Piston diameter [m]
+    Dh = 0.01f; //  Rod diameter [m]
+    L_cyl = 0.08f; // Stroke [m]
+    //L_cyl = 0.32f; // Stroke [m]
+    Vpl = 1.21E-3f; // Volume Pipeline [m^3]
+    In = 0.05f; //  Nominal valve input for Moog 24 [A]
+    pn = 70.0E+5f; // Nominal pressure drop for Moog 24 [Pa]
+    qn = 0.0001666f; // Nominal flow for Moog 24 [m^3/s]
+    
+
+    if(Mdes > 0){
+        double a_ADM[3] = {Mdes,Bdes,Kdes};
+        double b_ADM[3] = {0.0,0.0,1.0};   
+        transferFunction = new utility::AnalogFilter(2, a_ADM, b_ADM);
+    }
+
+    else {
+        double a_ADM[2] = {Bdes,Kdes};
+        double b_ADM[2] = {0.0,1.0};   
+        transferFunction = new utility::AnalogFilter(1, a_ADM, b_ADM); 
+    }
+
+
+    logs.push_back(&reference);
+    
+}
+
+float CompliantHelio::ForceController(const IHardware *hw, float ref){
+    float tau = hw->get_tau_s(0);
+
+    float deriv_force_desejada = (2.45*ref - 6*prev_ref_1 + 7.5*prev_ref_2 - 6.66*prev_ref_3 
+    + 3.75*prev_ref_4 - 1.2*prev_ref_5 + 0.16*prev_ref_6)/
+    (hw->get_dt());
+
+    prev_ref_6 = prev_ref_5;
+    prev_ref_5 = prev_ref_4;
+    prev_ref_4 = prev_ref_3;
+    prev_ref_3 = prev_ref_2;
+    prev_ref_2 = prev_ref_1;
+    prev_ref_1 = ref;
+
+    float deriv_force = hw->get_d_tau_s(0);
+
+    Pa = hw->get_pressure(3)*100000;
+    Pb = hw->get_pressure(2)*100000;
+    Ps = 16000000;
+    Pt = 0;
+    Pt = 0; // Sensor de pressão com problema
+
+    if(Pa == Ps){
+        Pa = Ps*0.99;
+    }
+
+    if(Pb == Ps){
+        Pb = Ps*0.99;
+    }
+
+    ixv = last_out - 0.0250*0;
+    //ixv = last_out;
+    //Corrigir a leitura da corrente para checar qual equação de g utilizar
+
+    err = ref - tau;
+    derr = (err - errPast) / hw->get_dt();
+
+    derr = (2.45*err - 6*prev_erro_1 + 7.5*prev_erro_2 - 6.66*prev_erro_3 
+    + 3.75*prev_erro_4 - 1.2*prev_erro_5 + 0.16*prev_erro_6)/
+    (hw->get_dt());
+
+    derr = lowPass->process(derr,hw->get_dt());
+
+    prev_erro_7 = prev_erro_6;
+    prev_erro_6 = prev_erro_5;
+    prev_erro_5 = prev_erro_4;
+    prev_erro_4 = prev_erro_3;
+    prev_erro_3 = prev_erro_2;
+    prev_erro_2 = prev_erro_1;
+    prev_erro_1 = err;
+
+    float dist_gain_max = max_g*max_disturb_current;
+    float dist_gain_min = min_g*min_disturb_current;
+
+    float dist_gain_med = dist_gain_max/2 + dist_gain_min/2;
+
+
+
+    ierr += err * hw->get_dt();
+    errPast = err;
+
+    float De2 = pow(De, 2);
+    float Dh2 = pow(Dh, 2);
+
+    Aa = (M_PI*(De2))/4;
+    Ab = ((M_PI*(De2))/4) - ((M_PI*(Dh2))/4);
+    Ap = Aa;                    
+    alfa = Ab/Aa;
+    Kv = qn/(In*sqrt(pn/2));
+
+    Va = Vpl + Aa*(x);
+    Vb = Vpl + (L_cyl - x)*Ab;
+
+    if(ixv >= 0.00000f){
+        g = Be*Aa*Kv*(round((Ps-Pa)/abs(Ps-Pa))*sqrt(abs(Ps-Pa))/Va + alfa*round((Pb-Pt)/abs(Pb-Pt))*sqrt(abs(Pb-Pt))/Vb);
+        }
+        
+    else{
+        g = Be*Aa*Kv*(round((Pa-Pt)/abs(Pa-Pt))*sqrt(abs(Pa-Pt))/Va + alfa*round((Ps-Pb)/abs(Ps-Pb))*sqrt(abs(Ps-Pb))/Vb);
+        }
+
+    //g = Be*Aa*Kv*( round((Pa-Pt)/abs(Pa-Pt))*sqrt(abs(Pa-Pt))/Va + alfa*round((Ps-Pb)/abs(Ps-Pb))*sqrt(abs(Ps-Pb))/Vb );
+
+
+    f = -Be*pow(Aa,2)*(pow(alfa,2)/Vb + 1/Va)*dx;
+
+    beta = sqrt(max_g/min_g);
+    gain_g_med = sqrt(max_g*min_g);
+    gain_f_med = (max_f + min_f)/2;
+
+    deriv_force_desejada = (2.45*ref - 6*prev_ref_1 + 7.5*prev_ref_2 - 6.66*prev_ref_3 
+    + 3.75*prev_ref_4 - 1.2*prev_ref_5 + 0.16*prev_ref_6)/
+    (hw->get_dt());
+
+
+    deriv_force_desejada = (ref - prev_ref_1)/ (hw->get_dt());
+
+    deriv_force_desejada = lowPassD->process(deriv_force_desejada, hw->get_dt());
+
+    prev_ref_6 = prev_ref_5;
+    prev_ref_5 = prev_ref_4;
+    prev_ref_4 = prev_ref_3;
+    prev_ref_3 = prev_ref_2;
+    prev_ref_2 = prev_ref_1;
+    prev_ref_1 = ref;
+
+    float u = (deriv_force_desejada - gain_f_med*f + disturb_model_gain*dist_gain_med*g + kp*(ref - tau) + ki*ierr + kd*derr);
+
+    float k = (beta*(abs((max_f - gain_f_med)*(f)) + etta) + (beta - 1)*abs(u) + beta*(abs(dist_gain_max - dist_gain_med)*(g)));
+
+    float sat_ = 0;
+    float s = tau - ref;
+
+    if(abs(s/psi) <= 1){
+        sat_ = s/psi;
+        }
+    else{
+        if(s >= 0){
+           sat_ = 1; 
+        }
+        else if(s < 0){
+            sat_ = -1; 
+        }
+    }
+    
+
+    //current = 1/(0.86*g)*(u - k*sign(s));
+    out = ((u - k*sat_)*1000)/(gain_g_med*g);
+
+    if(out > limit){
+        out = limit;
+    }
+    else if(out < -limit){
+        out = -limit;
+    }
+
+    //*(hw->var4) = out;
+    //out = lowPass->process(out,hw->get_dt());
+
+    last_out = out;
+
+    *(hw->var1) = erro_imp;
+    *(hw->var2) = deriv_erro_imp;
+    *(hw->var3) = ref;
+    *(hw->var4) = reference;
+    *(hw->var5) = x_hat;
+    *(hw->var6) = dx_hat;
+    *(hw->var7) = ddx_hat;
+
+
+    return out*gain_out;
+
+}
+
+float CompliantHelio::process(const IHardware *hw, std::vector<float> ref)
+{
+    //Kvc = Kvc*0.089;
+    //Kpc = Kpc*0.089;
+    reference = ref[0];
+    
+    tau = hw->get_tau_s(1);
+    dtau = hw->get_d_tau_s(1);
+
+    x = hw->get_theta(1);
+    dx = hw->get_d_theta(1);
+    ddx = hw->get_dd_theta(1);
+
+    
+
+    x_hat = (transferFunction->process(tau,hw->get_dt()) + ref[0]);
+
+    dx_hat = (x_hat - last_x_hat)/(hw->get_dt());
+
+    dx_hat = lowPassD_Xhat->process(dx_hat,hw->get_dt());
+
+    last_x_hat = x_hat;
+
+    ddx_hat = (dx_hat - last_dx_hat)/hw->get_dt();
+
+    ddx_hat = lowPassDD_Xhat->process(ddx_hat,hw->get_dt());
+
+    last_dx_hat = dx_hat;
+
+
+
+
+
+
+    z = x - x_hat;  
+
+    dz = (z - last_z)/(hw->get_dt());
+
+    dz = lowPassD_z->process(dz,hw->get_dt());
+
+    last_z = z;
+
+
+
+    float sat_ = 0;
+    float s = dz;
+
+    if(abs(s/psi_compliant) <= 1){
+        sat_ = s/psi_compliant;
+        }
+    else{
+        if(s >= 0){
+           sat_ = 1; 
+        }
+        else if(s < 0){
+            sat_ = -1; 
+        }
+    }
+
+
+    new_forca_desejada = -K1*z - K2*dz - tau - F_fric*sat_ + massa_total*ddx_hat;
+
+
+    erro_imp = x - ref[0];
+
+    deriv_erro_imp = (erro_imp - last_erro_imp)/hw->get_dt();
+
+    last_erro_imp = erro_imp;
+
+    deriv_erro_imp = lowPassD_ErroImp->process(deriv_erro_imp,hw->get_dt());
+
+    float tau_ref = - Kdes*(erro_imp) -  Bdes*deriv_erro_imp - Mdes*ddx;
+
+
+
+    float out = ForceController(hw,new_forca_desejada);
+
+    return out;
+}
